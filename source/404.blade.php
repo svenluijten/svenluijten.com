47f1e795ae1d917a46bdee7b70458f6a88b55134
Sven Luijten <contact@svenluijten.com>
2023-07-01 23:23:42 +0200

    Add custom 404 page

---
+++ source/404.blade.php
@@ -0,0 +1,15 @@
+---
+permalink: 404.html
+---
+
+@extends('_layouts.main')
+
+@section('content')
+    <div class="bg-indigo-50 p-4 shadow rounded">
+        <h1 class="font-extrabold text-4xl border-b border-indigo-100 pb-2 mb-2">That's a 404!</h1>
+
+        <p>
+            Looks like you took a wrong turn somewhere. Care to <a href="{{ $page->link('/') }}">go back home</a>?
+        </p>
+    </div>
+@endsection
