4301b7edb722f60ee6dcd205998bcc13dec34a20
Sven Luijten <contact@svenluijten.com>
2023-07-03 21:19:16 +0200

    Rename devPosts to posts

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Carbon\CarbonImmutable;
+use TightenCo\Jigsaw\Collection\CollectionItem;
+
+class Post extends CollectionItem
+{
+    use HasReadingTime;
+    use HasDate;
+
+    public function previous()
+    {
+        return $this->collection
+            ->sortByDesc(fn(Post $item) => (new CarbonImmutable($item->date))->timestamp)
+            ->first(fn(Post $item) => (new CarbonImmutable($item->date))->lessThan(new CarbonImmutable($this->date)));
+    }
+
+    public function next()
+    {
+        return $this->collection
+            ->sortByDesc(fn(Post $item) => (new CarbonImmutable($item->date))->timestamp)
+            ->last(fn(Post $item) => (new CarbonImmutable($item->date))->greaterThan(new CarbonImmutable($this->date)));
+    }
+}
