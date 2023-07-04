5640924a51f905bd19ea948c910a59c88e875af8
Sven Luijten <contact@svenluijten.com>
2023-07-04 16:21:41 +0200

    Fix Atom validation errors

--- source/_partials/atom-entry.blade.php
+++ source/_partials/atom-entry.blade.php
@@ -1,10 +1,11 @@
 <entry>
     <id>{{ $entry->getUrl() }}</id>
     <title>{{ $entry->title }}</title>
-    <updated>{{ (new \Carbon\Carbon($entry->updated ?? $entry->date))->format('Y-m-d\TH:i:s\Z') }}</updated>
+    <summary>{{ $entry->excerpt ?? $entry->summary }}</summary>
+    <updated>{{ (new \Carbon\Carbon($entry->updated ?? $entry->date))->format('Y-m-d\T00:00\Z') }}</updated>
     <author>
         <name>Sven Luijten</name>
     </author>
     <link href="{{ $entry->getUrl() }}" rel="alternate" type="text/html" />
-    <published>{{ (new \Carbon\Carbon($entry->date))->format('Y-m-d') }}</published>
+    <published>{{ (new \Carbon\Carbon($entry->date))->format('Y-m-d\T00:00\Z') }}</published>
 </entry>
