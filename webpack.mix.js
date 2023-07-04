cd18bd42931d1d8d44c3962a9f91e152abb40c00
Sven Luijten <11269635+svenluijten@users.noreply.github.com>
2023-07-04 18:45:40 +0200

    Relative markdown links (#6)
    * Watch 'app' directory
    
    * Make relative markdown links work
    
    * Ignore built assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,12 @@ require('laravel-mix-jigsaw');
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
+mix.jigsaw({
+    watch: {
+        dirs: ['app/*/']
+    },
+});
+
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
