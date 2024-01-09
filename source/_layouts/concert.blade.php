90426b82ab5602124d3c15035ab57000bff6e276
Sven Luijten <contact@svenluijten.com>
2024-01-09 22:37:56 +0100

    Wip

--- source/_layouts/concert.blade.php
+++ source/_layouts/concert.blade.php
@@ -10,7 +10,9 @@
             </span>
         </x-page-header>
 
-        <section id="post-content" class="prose lg:prose-xl">
+        <hr class="my-6 | dark:border-gray-950">
+
+        <section id="post-content" class="prose lg:prose-xl dark:prose-invert">
             @yield('body')
         </section>
 
--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -6,7 +6,9 @@
     <article>
         <x-page-header :page="$page" />
 
-        <section id="post-content" class="prose-lg">
+        <hr class="my-6 | dark:border-gray-950">
+
+        <section id="post-content" class="prose lg:prose-xl dark:prose-invert">
             @yield('body')
         </section>
 
