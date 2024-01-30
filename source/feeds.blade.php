ed85ccb06cd62d8f4ceb6e52d382bca772e21d51
Sven Luijten <contact@svenluijten.com>
2024-01-30 21:48:01 +0100

    Wip

--- source/feeds.blade.php
+++ source/feeds.blade.php
@@ -6,7 +6,7 @@
     <h1 class="sr-only">Feeds</h1>
 
     <p class="mb-4">
-        I'm a staunch believer in owning your own content and distributing it via open standards, which is why all my
+        I believe everyone should own their content and distribute it via open standards, which is why all my
         content is published in Atom and RSS feeds. Below is an overview of the feeds this site currently supports:
     </p>
 
@@ -26,5 +26,4 @@
             My concert logs entries.
         </li>
     </ul>
-
 @endsection
