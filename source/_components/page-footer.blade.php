fb0ba8d441bf2f9174d5ee49f2e473094daef61e
Sven Luijten <contact@svenluijten.com>
2024-01-30 22:14:42 +0100

    Make post navigation look not dumb

--- source/_components/page-footer.blade.php
+++ source/_components/page-footer.blade.php
@@ -7,20 +7,26 @@
 
     <nav role="menu" aria-label="Previous and next post(s) if available" class="flex flex-col md:flex-row justify-between gap-4">
         @if ($page->previous())
-            <a href="{{ $page->previous()->getUrl() }}" class="flex-1 block border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group">
-                <div class="text-xs text-gray-500 text-left">
-                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&lsaquo;</span> Previous
+            <a href="{{ $page->previous()->getUrl() }}" class="flex-1 border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
+                <div class="text-xl text-gray-500 text-left self-center">
+                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&lsaquo;</span>
                 </div>
-                {{ $page->previous()->title }}
+                <div class="flex flex-col text-right">
+                     <span class="text-gray-500">Previous</span>
+                    {{ $page->previous()->title }}
+                </div>
             </a>
         @endif
 
         @if ($page->next())
-            <a href="{{ $page->next()->getUrl() }}" class="flex-1 block border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group">
-                <div class="text-xs text-gray-500 text-right">
-                    Next <span class="group-hover:text-indigo-500 group-hover:font-bold">&rsaquo;</span>
+            <a href="{{ $page->next()->getUrl() }}" class="flex-1 border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
+                <div class="flex flex-col text-left">
+                    <span class="text-gray-500">Next</span>
+                    {{ $page->next()->title }}
                 </div>
-                {{ $page->next()->title }}
+                <div class="text-xl text-gray-500 text-right self-center">
+                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&rsaquo;</span>
+                </div>
             </a>
         @endif
     </nav>
