4e0368c155f60da5d3d02ced44c452c1ef8be82e
Sven Luijten <contact@svenluijten.com>
2024-01-30 17:49:16 +0100

    Style post footer, increate touch target sizes

--- source/_components/page-footer.blade.php
+++ source/_components/page-footer.blade.php
@@ -1,20 +1,29 @@
 @props(['title', 'page'])
 
-<footer>
-    <h1 id="{{ str($title)->kebab() }}">{{ $title }}</h1>
-    <nav aria-label="Previous and next post(s) if available">
-        <ul role="menu" aria-labelledby="{{ str($title)->kebab() }}">
-            @if ($page->previous())
-                <li>
-                    Previous: <a href="{{ $page->previous()->getUrl() }}">{{ $page->previous()->title }}</a>
-                </li>
-            @endif
+<footer class="my-4">
+    <x-separator color="indigo-700" />
 
-            @if ($page->next())
-                <li>
-                    Next: <a href="{{ $page->next()->getUrl() }}">{{ $page->next()->title }}</a>
-                </li>
-            @endif
-        </ul>
+    <h1 class="text-sm text-center uppercase text-gray-500 mb-4">
+        {{ $title }}
+    </h1>
+
+    <nav role="menu" aria-label="Previous and next post(s) if available" class="flex flex-col md:flex-row justify-between gap-4">
+        @if ($page->previous())
+            <a href="{{ $page->previous()->getUrl() }}" class="flex-1 block border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group">
+                <div class="text-xs text-gray-500 text-left">
+                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&lsaquo;</span> Previous
+                </div>
+                {{ $page->previous()->title }}
+            </a>
+        @endif
+
+        @if ($page->next())
+            <a href="{{ $page->next()->getUrl() }}" class="flex-1 block border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group">
+                <div class="text-xs text-gray-500 text-right">
+                    Next <span class="group-hover:text-indigo-500 group-hover:font-bold">&rsaquo;</span>
+                </div>
+                {{ $page->next()->title }}
+            </a>
+        @endif
     </nav>
 </footer>
