fa68f23e6ce36d82d399c943a31f4a1f1d510f43
Sven Luijten <contact@svenluijten.com>
2024-02-01 19:14:39 +0100

    Put classes on parent

--- source/_components/page-footer.blade.php
+++ source/_components/page-footer.blade.php
@@ -8,9 +8,7 @@
     <nav role="menu" aria-label="Previous and next post(s) if available" class="flex flex-col md:flex-row justify-between gap-4">
         @if ($page->previous())
             <a href="{{ $page->previous()->getUrl() }}" class="flex-1 border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
-                <div class="text-xl text-gray-500 text-left self-center">
-                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&lsaquo;</span>
-                </div>
+                <div class="text-xl text-gray-500 text-left self-center group-hover:text-indigo-500 group-hover:font-bold mr-2">&lsaquo;</div>
                 <div class="flex flex-col text-right">
                      <span class="text-gray-500">Previous</span>
                     {{ $page->previous()->title }}
@@ -24,9 +22,7 @@
                     <span class="text-gray-500">Next</span>
                     {{ $page->next()->title }}
                 </div>
-                <div class="text-xl text-gray-500 text-right self-center">
-                    <span class="group-hover:text-indigo-500 group-hover:font-bold">&rsaquo;</span>
-                </div>
+                <div class="text-xl text-gray-500 text-right self-center group-hover:text-indigo-500 group-hover:font-bold ml-2">&rsaquo;</div>
             </a>
         @endif
     </nav>
