230ba4d8f42a235a6d89eee7ac54a22aaf2b7362
Sven Luijten <contact@svenluijten.com>
2024-02-01 21:25:39 +0100

    Fixes

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -8,14 +8,14 @@
     @foreach ($posts as $post)
         <a href="{{ $post->getUrl() }}" id="{{ $post->id() }}" class="group block py-2 my-2 px-4 -mx-4">
             <article aria-labelledby="{{ $post->id() }}" class="no-underline">
-                <h2 class="relative text-xl">
+                <h2 class="relative text-xl link">
                     <span class="group-hover:no-underline underline">{{ $post->title }}</span>
                     <span class="absolute -top-0.5 -left-3.5 font-bold invisible group-hover:visible text-indigo-700" aria-hidden="true">&rsaquo;</span>
                 </h2>
 
                 <p>{{ $post->excerpt }}</p>
 
-                <footer class="text-gray-600 text-sm">
+                <footer class="text-gray-600 dark:text-indigo-200 text-sm">
                     <time datetime="{{ $post->getDate('Y-m-d H:i:s') }}" title="{{ $post->getDate('Y-m-d') }}">
                         {{ $post->getDate('F jS, Y') }}
                     </time>
@@ -23,7 +23,7 @@
                     &bullet;
 
                     <span>
-                        About {{ $post->minutesToRead() }} {{ \Illuminate\Support\Str::plural('minute', $post->minutesToRead()) }} to read
+                        About {{ $post->minutesToRead() }} {{ str('minute')->plural($post->minutesToRead()) }} to read
                     </span>
                 </footer>
             </article>
