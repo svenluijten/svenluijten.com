9ecb06461a07d404bf2f7263e1ce469a41ef1ec2
Sven Luijten <contact@svenluijten.com>
2024-04-12 20:20:35 +0200

    Change colors of next/previous post buttons

--- source/_components/page-footer.blade.php
+++ source/_components/page-footer.blade.php
@@ -7,19 +7,20 @@
 
     <nav role="menu" aria-label="Previous and next post(s) if available" class="flex flex-col md:flex-row justify-between gap-4">
         @if ($page->previous())
-            <a href="{{ $page->previous()->getUrl() }}" class="flex-1 border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
+            <a href="{{ $page->previous()->getUrl() }}" class="bg-white dark:bg-gray-800 flex-1 border border-indigo-300 dark:border-indigo-700 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
                 <div class="text-xl text-gray-500 text-left self-center group-hover:text-indigo-500 group-hover:font-bold mr-2">&lsaquo;</div>
                 <div class="flex flex-col text-right">
-                     <span class="text-gray-500 font-sans">Previous</span>
+                     <span class="text-gray-500 dark:text-gray-400 font-sans">Previous</span>
+
                     {{ $page->previous()->title }}
                 </div>
             </a>
         @endif
 
         @if ($page->next())
-            <a href="{{ $page->next()->getUrl() }}" class="flex-1 border border-indigo-300 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
+            <a href="{{ $page->next()->getUrl() }}" class="bg-white dark:bg-gray-800 flex-1 border border-indigo-300 dark:border-indigo-700 hover:border-indigo-600 py-2 px-4 rounded group | flex justify-between items-start">
                 <div class="flex flex-col text-left">
-                    <span class="text-gray-500 font-sans">Next</span>
+                    <span class="text-gray-500 dark:text-gray-400 font-sans">Next</span>
                     {{ $page->next()->title }}
                 </div>
                 <div class="text-xl text-gray-500 text-right self-center group-hover:text-indigo-500 group-hover:font-bold ml-2">&rsaquo;</div>
