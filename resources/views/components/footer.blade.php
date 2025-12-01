1bf0120312c773799aa781dad90e5bbf56ee30a3
Sven Luijten <contact@svenluijten.com>
2025-12-01 20:45:21 +0100

    Refine footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,17 +2,17 @@
     <div class="container mx-auto">
         <div class="mx-auto w-full flex flex-row justify-between py-4 px-6 | lg:w-2/3 lg:px-0">
             <div>
-                <a href="{{ route('home') }}">Sven Luijten</a>
-                <span class="text-black | dark:text-indigo-100">&copy; {{ now()->format('Y') }}</span>
+                <a href="{{ route('home') }}" class="link">Sven Luijten</a>
+                <span>&copy; {{ now()->format('Y') }}</span>
             </div>
 
             <nav class="flex | sm:mt-0">
-                <a href="https://github.com/svenluijten" target="_blank" rel="noreferrer me author">
+                <a href="https://github.com/svenluijten" target="_blank" rel="noreferrer me author" class="link">
                     GitHub
                 </a>
 
-                <a href="/feeds/all.xml" class="ml-4">
-                    RSS
+                <a href="/feeds" class="ml-4 link">
+                    Feeds
                 </a>
             </nav>
         </div>
