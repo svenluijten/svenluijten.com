6923bc198131852b9bbda514a112653f815b8853
Sven Luijten <contact@svenluijten.com>
2025-12-09 14:51:23 +0100

    Add ability to bulk-update concerts' venues

--- app/Filament/Resources/Concerts/Tables/ConcertsTable.php
+++ app/Filament/Resources/Concerts/Tables/ConcertsTable.php
@@ -2,11 +2,16 @@
 
 namespace App\Filament\Resources\Concerts\Tables;
 
+use App\Models\Venue;
+use Filament\Actions\BulkAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Collection;
 
 class ConcertsTable
 {
@@ -51,6 +56,26 @@ class ConcertsTable
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
+                    BulkAction::make('Update venue')
+                        ->schema([
+                            Select::make('venue_id')
+                                ->label('Venue')
+                                ->relationship('venue', 'name')
+                                ->createOptionForm([
+                                    TextInput::make('name')
+                                        ->required(),
+                                    TextInput::make('country')
+                                        ->required(),
+                                    TextInput::make('city')
+                                        ->required(),
+                                ])
+                                ->searchable(),
+                        ])
+                        ->action(function (Collection $records, array $data) {
+                            $records->each->update([
+                                'venue_id' => $data['venue_id'],
+                            ]);
+                        })
                 ]),
             ]);
     }
