07c8fbccb183aa5df28cd2c18870fc266627470e
Sven Luijten <contact@svenluijten.com>
2025-12-22 21:53:14 +0100

    Cleanup

--- app/Filament/Resources/Articles/Tables/ArticlesTable.php
+++ app/Filament/Resources/Articles/Tables/ArticlesTable.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Articles\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -10,8 +10,8 @@ class Home
     public function __invoke()
     {
         return view('index', [
-            'recentArticles' => Article::latest('published_at')->take(5)->get(),
-            'recentConcerts' => Concert::latest('date')->take(4)->get(),
+            'recentArticles' => Article::query()->latest('published_at')->take(5)->get(),
+            'recentConcerts' => Concert::query()->latest('date')->take(4)->get(),
         ]);
     }
 }
