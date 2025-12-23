af9ab19b6c0ddedebbe5841472080ae8f531bd63
Sven Luijten <contact@svenluijten.com>
2025-12-23 13:37:48 +0100

    Look and feel

--- resources/views/components/meta/look-feel.blade.php
+++ resources/views/components/meta/look-feel.blade.php
@@ -1,21 +1,9 @@
 <!-- Look & Feel -->
-<link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" href="{{ url('/images/favicons/favicon-96x96.png') }}" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ url('/images/favicons/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ url('/images/favicons/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ url('/images/favicons/apple-touch-icon.png') }}" />
+<meta name="apple-mobile-web-app-title" content="Sven Luijten" />
+<link rel="manifest" href="{{ url('/images/favicons/site.webmanifest') }}" />
 
-<link rel="icon" type="image/png" sizes="192x192" href="/assets/icons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
-
-<link rel="manifest" href="/manifest.json">
-
-<meta name="msapplication-TileColor" content="#4f46e5">
-<meta name="msapplication-TileImage" content="/assets/icons/ms-icon-144x144.png">
-<meta name="theme-color" content="#4f46e5">
+<meta name="theme-color" content="#627254">
