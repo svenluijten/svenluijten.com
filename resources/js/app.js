c249e6a5010bd0fca42c56ad80ec3282e7ec1048
Sven Luijten <contact@svenluijten.com>
2025-12-29 19:24:06 +0100

    Replace highlight.js with Phiki

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,3 @@
-import hljs from "highlight.js";
-import dockerfile from "highlight.js/lib/languages/dockerfile";
 import './components/image-lightbox.js';
 import './components/image-carousel.js';
 
-hljs.registerLanguage('dockerfile', dockerfile);
-
-(function () {
-    hljs.highlightAll();
-})();
