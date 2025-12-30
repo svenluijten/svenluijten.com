353bcc3cee78a8a3bd59ba1cd1c32eab2ca8dfe8
Sven Luijten <contact@svenluijten.com>
2025-12-30 17:12:11 +0100

    Allow showing of blog page before it's published

--- app/Http/Controllers/BlogPosts/Show.php
+++ app/Http/Controllers/BlogPosts/Show.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\BlogPosts;
 
 use App\Models\BlogPost;
+use App\Models\Scopes\PublishedScope;
 use Illuminate\Contracts\View\View;
 
 class Show
 {
-    public function __invoke(BlogPost $blogPost): View
+    public function __invoke(string $blogPost): View
     {
+        $post = BlogPost::query()
+            ->withoutGlobalScope(PublishedScope::class)
+            ->where('slug', $blogPost)
+            ->firstOrFail();
+
         return view('blog-posts.show', [
-            'blogPost' => $blogPost,
+            'blogPost' => $post,
         ]);
     }
 }
