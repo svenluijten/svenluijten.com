dcebc93da0cf6e92b3858cf8cfe905560ad751d0
Sven Luijten <contact@svenluijten.com>
2025-12-30 16:56:21 +0100

    Render entirely different view

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\BlogPost;
 use App\Models\Concert;
+use Laravel\Pennant\Feature;
 
 class Home
 {
     public function __invoke()
     {
-        return view('index', [
+        $view = match (Feature::active('blog')) {
+            true => 'index-with-blog',
+            default => 'index',
+        };
+
+        return view($view, [
             'recentArticles' => Article::query()->latest('published_at')->take(5)->get(),
             'recentConcerts' => Concert::query()->latest('date')->take(4)->get(),
+            'recentBlogPosts' => BlogPost::query()->latest('published_at')->take(5)->get(),
         ]);
     }
 }
--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -15,13 +15,23 @@
         just a few blurry pictures and videos, I write about my experience there.
     </x-section>
 
+    @feature('blog')
+        <x-section title="Blog">
+            I write on <a href="{{ route('blog.index') }}" class="link">my blog</a> about topics that interest me in that moment.
+            These random thoughts might are used to generate new ideas, help me find like-minded people online, and clarify
+            my thinking about any given subject. Topics might range from development tips or things I've learned, to thoughts
+            on an article or other blog post I read online, to just about anything I'm thinking about at the time.
+        </x-section>
+    @endfeature
+
     <x-section title="Feeds">
         <p>This site automatically publishes a handful of feeds for every content type, as well as a generic one that contains everything.</p>
 
         <ul class="list-disc my-4 ml-4">
-            <li><a href="/feeds/all.xml" class="link"><code class="code">/feeds/all.xml</code></a> contains all content published on this site.</li>
-            <li><a href="/feeds/articles.xml" class="link"><code class="code">/feeds/articles.xml</code></a> contains only the articles I write.</li>
-            <li><a href="/feeds/concerts.xml" class="link"><code class="code">/feeds/concerts.xml</code></a> only contains the concert logs.</li>
+            <li><a href="{{ url('/feeds/all.xml') }}" class="link"><code class="code">/feeds/all.xml</code></a> contains all content published on this site.</li>
+            <li><a href="{{ url('/feeds/articles.xml') }}" class="link"><code class="code">/feeds/articles.xml</code></a> contains only the articles I write.</li>
+            <li><a href="{{ url('/feeds/concerts.xml') }}" class="link"><code class="code">/feeds/concerts.xml</code></a> only contains the concert logs.</li>
+            <li><a href="{{ url('/feeds/blog-posts.xml') }}" class="link"><code class="code">/feeds/blog-posts.xml</code></a> only contains my blog posts.</li>
         </ul>
 
         <p>This same list can be found on <a href="{{ route('feeds.index') }}" class="link">the dedicated <code class="code">/feeds</code> page</a>.</p>
